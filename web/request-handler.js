c94975da8c78b87a0a36b1f4bf67f67048ec83cb
sunyang730 <sunyang730@gmail.com>
2015-02-25 15:01:29 -0800

    Add: fs.readFile for static page display

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,18 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  fs.readFile(archive.paths.siteAssets + '/index.html', function (err, data){
+    if (err){
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    //Data is HTML file. convert data to string with data.toString();
+    res.end(data);
+  });
+
 };
